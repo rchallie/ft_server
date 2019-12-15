ea382dad1dfabc3a8ba89aa6b7efd34d3fae7d46
Romain Challier <rchallie@student.42.fr>
2019-12-15 17:23:19 +0100

    Update init_container.sh

--- srcs/init_container.sh
+++ srcs/init_container.sh
@@ -19,7 +19,7 @@ rm -rf /etc/nginx/sites-enabled/default
 
 # Config MYSQL
 echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
-echo "CREATE USER 'wp'@'localhost' IDENTIFIED BY 'wordpress'; " | mysql -u root --skip-password
+# echo "CREATE USER 'wp'@'localhost' IDENTIFIED BY 'wordpress'; " | mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
