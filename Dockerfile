ef236421eab48f0db8e825ad6ba7f64adc83f174
Romain Challier <rchallie@student.42.fr>
2020-01-02 11:17:26 +0100

    need to be corrected

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,11 @@ RUN apt-get update && apt-get install -y procps && apt-get install nano && apt-g
 RUN apt-get -y install php7.3-fpm php7.3-common php7.3-mysql php7.3-gmp php7.3-curl php7.3-intl php7.3-mbstring php7.3-xmlrpc php7.3-gd php7.3-xml php7.3-cli php7.3-zip php7.3-soap php7.3-imap
 RUN apt-get -y install wget
 RUN apt-get -y install nginx
-RUN apt-get -y install default-mysql-server
+RUN apt-get -y install mariadb-server
 
-COPY ./srcs/nginx-conf /root/nginx-conf
-COPY ./srcs/phpmyadmin.inc.php /root/phpmyadmin.inc.php
 COPY ./srcs/init_container.sh ./
-COPY ./srcs/wp-config.php ./tmp
+COPY ./srcs/nginx-conf ./tmp/nginx-conf
+COPY ./srcs/phpmyadmin.inc.php ./tmp/phpmyadmin.inc.php
+COPY ./srcs/wp-config.php ./tmp/wp-config.php
 
 CMD bash init_container.sh
--- srcs/init_container.sh
+++ srcs/init_container.sh
@@ -13,22 +13,21 @@ mkdir /etc/nginx/ssl
 openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/monsupersite.pem -keyout /etc/nginx/ssl/monsupersite.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=rchallie/CN=monsupersite"
 
 # Config NGINX
-cp /root/nginx-conf /etc/nginx/sites-available/monsupersite
+mv ./tmp/nginx-conf /etc/nginx/sites-available/monsupersite
 ln -s /etc/nginx/sites-available/monsupersite /etc/nginx/sites-enabled/monsupersite
 rm -rf /etc/nginx/sites-enabled/default
 
 # Config MYSQL
 echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
-# echo "CREATE USER 'wp'@'localhost' IDENTIFIED BY 'wordpress'; " | mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
-echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
 # DL phpmyadmin
 mkdir /var/www/monsupersite/phpmyadmin
 wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
 tar -xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz --strip-components 1 -C /var/www/monsupersite/phpmyadmin
-mv /root/phpmyadmin.inc.php /var/www/monsupersite/phpmyadmin/config.inc.php
+mv ./tmp/phpmyadmin.inc.php /var/www/monsupersite/phpmyadmin/config.inc.php
 
 # DL wordpress
 cd /tmp/
